33c9936aea185d7b18dee4b23d8643e3d7f3b770
jrj-sys <jamesjamel99@gmail.com>
2022-06-22 01:35:49 -0500

    mutations and JWT

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,6 +3,7 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
+
   type User {
     _id: ID
     username: String
@@ -11,6 +12,12 @@ const typeDefs = gql`
     thoughts: [Thought]
     friends: [User]
   }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Thought {
     _id: ID
     thoughtText: String
@@ -28,11 +35,21 @@ const typeDefs = gql`
   }
 
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
   }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
+
   `;
 
 
