47503784a7af9add4dc417de93db88d9e2443636
jrj-sys <jamesjamel99@gmail.com>
2022-06-24 01:19:06 -0500

    rm pd

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;2022 by Jamel James</div>
+      <div className="container">&copy;2022 by <a href="https://github.com/jrj-sys" target="_blank">jrj-sys</a></div>
     </footer>
   );
 };
